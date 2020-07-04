2737e080ed57e079b55bd17e2577ccd58eeed636
Christian Gimber <cgimber@ucla.edu>
2020-07-04 03:23:52 -0700

    initial commit

--- js/main.js
+++ js/main.js
@@ -3,7 +3,15 @@
 
 /* document ready
 ---------------------------------------------------------------------*/
-$(document).ready(function() {});
+$(document).ready(function() {
+    typer('.dialog__message')
+        .pause()
+        .cursor({ blink: 'hard' }).line({
+            container: '.dialog__message--hidden',
+            min: 30,
+            max: 60
+        });
+});
 
 /* functions
 ---------------------------------------------------------------------*/
